c3d09684ab31e5240ff6796ede78d6977db3a92f
marufurrahman <marufurrahman1997@gmail.com>
2018-09-26 23:34:05 +0600

    while loop

---
+++ while_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+    using namespace std;
+    int n = 1;
+    while( n <= 10){
+        cout << n << endl;
+        n++;
+    }
+    cout << "The loop is over!";
+    return 0;
+}
