3ecf8335b04b615a321e93a5e23eb5a730657136
Marufur Rahman <marufurrahman1997@gmail.com>
2018-10-02 15:56:32 +0600

    Update math1.cpp

--- math1.cpp
+++ math1.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main()
 {
-    cout << fmax(10, 11) << endl; // Find the max number
+    cout << fmax(10, 11) << endl; // Find the big number
+    cout << fmin(10, 11) << endl; // Find the small number
     cout << pow(5, 2) << endl; // Do power of them
     return 0;
 }
