c9c07a791292225ae4cd5ba7eef831c14dd79895
marufurrahman <marufurrahman1997@gmail.com>
2018-10-02 15:09:05 +0600

    initial commit

---
+++ data_types.cpp
@@ -0,0 +1,22 @@
+// Playing with data types
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string name = "Marufur Rahman";
+    int age = 21;
+    double gpa = 4.1;
+    bool isMale = true;
+    
+    cout << name << endl;
+    cout << age << endl;
+    cout << grade << endl;
+    cout << gpa << endl;
+    cout << isMale << endl;
+    
+    return 0;
+}
