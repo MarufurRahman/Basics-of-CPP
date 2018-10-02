f1514ed872b815ab3acdbe80733025ddc8af6c31
marufurrahman <marufurrahman1997@gmail.com>
2018-10-02 22:53:52 +0600

    functions1

---
+++ create_and_call_function.cpp
@@ -0,0 +1,17 @@
+// Programmed by Marufur Rahman
+
+#include <iostream>
+
+using namespace std;
+
+// Create a function
+void sayHello(){
+	cout << "Hello!" << endl;
+}
+
+int main()
+{
+	sayHello(); // Call the function
+
+	return 0;
+}
