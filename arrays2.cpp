b1a8a00fdc7956ccdb65917dc6fb6507a93d6120
marufurrahman <marufurrahman1997@gmail.com>
2018-10-03 22:39:26 +0600

    access index items

---
+++ arrays2.cpp
@@ -0,0 +1,14 @@
+// Arrays example
+
+#include <iostream>
+using namespace std;
+
+int foo[] = {1, 2, 3, 4};
+int bar[] = {12, 23, 43, 546};
+
+int main()
+{
+	cout << foo[0] << endl;
+
+	return 0;
+}
