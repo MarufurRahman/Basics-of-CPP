2d5725e16ea134e3d727ed3746b1a49cf233b3c5
marufurrahman <marufurrahman1997@gmail.com>
2018-10-06 14:15:06 +0600

    initial commit

---
+++ Codes/infinite_loop.cpp
@@ -0,0 +1,14 @@
+// Infinite loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for( ; ; ){
+		cout << "This loop will never end..." << endl;
+	}
+
+	return 0;
+}
---
+++ Codes/reverse_loop.cpp
@@ -0,0 +1,19 @@
+// Reverse integer number loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int index = 10;
+
+	while(index >= 1){
+		cout << index << endl;
+		index--;
+	}
+
+	cout << "Loop is finished.";
+
+	return 0;
+}
