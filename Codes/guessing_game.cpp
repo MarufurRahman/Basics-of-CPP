0553439f975c8770307394530904c8d066bc3571
marufurrahman <marufurrahman1997@gmail.com>
2018-10-07 11:16:51 +0600

    initial commit

---
+++ Codes/guessing_game.cpp
@@ -0,0 +1,21 @@
+// Guessing game
+// Programmed by Marufur Rahman
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int secretNum = 0;
+	int guess;
+
+	while(secretNum != guess){
+		cout << "Enter guess: ";
+		cin >> guess;
+	}
+
+	cout << "You Win!" << endl;
+
+	return 0;
+}
