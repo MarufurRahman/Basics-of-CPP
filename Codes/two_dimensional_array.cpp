e9cac488e6b8f72a7de825906620cdfcf93d6b6f
marufurrahman <marufurrahman1997@gmail.com>
2018-10-07 22:26:43 +0600

    Two Dimentional Array

---
+++ Codes/two_dimensional_array.cpp
@@ -0,0 +1,18 @@
+// Two dimensional array
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int twoArr[3][2] = {
+		{1, 2},
+		{3, 4},
+		{5, 6}
+	};
+
+	cout << twoArr[0][1];
+
+	return 0;
+}
